7c32e04beb9b65f95c81644e475badd93e476ee3
coolsks <47688809+sks-21@users.noreply.github.com>
2021-09-19 15:25:04 +0530

    added input validator fn

--- app.js
+++ app.js
@@ -9,41 +9,64 @@ function calcAmt(billAmt, cashGiven) {
   let msg = "";
   billAmt = parseInt(billAmt);
   cashGiven = parseInt(cashGiven);
-  if (billAmt > cashGiven) {
-    console.log(1);
-    msg = "Please provide more cash";
-  } else if (billAmt === cashGiven) {
-    msg = "Exact amount given. Nothing to return !";
-  } else {
-    let amt = cashGiven - billAmt;
-    let i = 0;
-    while (amt > 0) {
-      a = amt / currency[i]; //no of notes of that currency
-      b = amt % currency[i]; //left amount
 
-      a = Math.trunc(a);
-      if (a > 0) {
-        msg += `&#8377;${currency[i]} * ${a} notes <br>`;
-      }
-      amt = b;
-      i++;
+  let amt = cashGiven - billAmt;
+  let i = 0;
+
+  while (amt > 0) {
+    a = amt / currency[i]; //no of notes of that currency
+    b = amt % currency[i]; //left amount
+
+    a = Math.trunc(a);
+    if (a > 0) {
+      msg += `&#8377;${currency[i]} * ${a} notes <br>`;
     }
+    amt = b;
+    i++;
   }
 
   return msg;
 }
 
+//Validator function
+function inputValidator(bill, cash) {
+  let arr;
+  billVal = parseInt(bill);
+  cashVal = parseInt(cash);
+
+  if (bill === "" || cash === "") {
+    arr = [0, "Invalid values"];
+  } else if (billVal < 0 || cashVal < 0) {
+    arr = [0, "Values can not be -ve"];
+  } else if (billVal > cashVal) {
+    arr = [0, "Please provide more cash"];
+  } else if (billVal === cashVal) {
+    arr = [0, "Exact amount given. Nothing to return !"];
+  } else {
+    arr = [1, " "];
+  }
+
+  return arr;
+}
+
 form.elements.calculate.addEventListener("click", (e) => {
   e.preventDefault();
 
+  let ans;
   let billEl = form.elements.inputBill;
   let cashEl = form.elements.inputCash;
 
   let bill = billEl.value;
   let cash = cashEl.value;
 
+  //Input validator
+  let checkArr = inputValidator(bill, cash);
+  ans = checkArr[1];
+
   //program logic
-  let ans = calcAmt(bill, cash);
+  if (checkArr[0]) {
+    ans = calcAmt(bill, cash);
+  }
 
   billEl.value = "";
   cashEl.value = "";
